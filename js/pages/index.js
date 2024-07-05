16863c4e526dcfd6737cd5b7592979be701fd6da
Oscar Frischmann <oscarfrisc@gmail.com>
2024-07-05 19:30:49 -0300

    fabrica

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,17 +1,20 @@
-import { indexImagesDB } from '../panel.js';
-
-const categoriesCont = document.getElementById('categories');
+import { indexImagesDB } from "../panel.js";
+const categoriesCont = document.getElementById("categories");
 console.log(indexImagesDB);
 for (let key in await indexImagesDB) {
-	console.log(indexImagesDB[key]);
-	categoriesCont.innerHTML += `
+  console.log(indexImagesDB[key]);
+  categoriesCont.innerHTML += `
       <div class="col-sm p-2 position-relative">
-        <img src=${indexImagesDB[key]}>
+        <img src=${indexImagesDB[key]} id="img">
         <button class="main-type-title" id="mujer">${key}</button>
       </div>
     `;
 }
-const indexBTNs = document.querySelectorAll('.index button');
+
+const indexBTNs = document.querySelectorAll(".index button");
 indexBTNs.forEach((button, i) => {
-	button.setAttribute('onclick', `location.href ='./pages/${indexBTNs[i].attributes[1].nodeValue}.html'`);
+  button.setAttribute(
+    "onclick",
+    `location.href ='./pages/${indexBTNs[i].attributes[1].nodeValue}.html'`
+  );
 });
