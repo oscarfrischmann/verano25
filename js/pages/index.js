77dde499fabbbe3b07c538124439d50e8aea6ac3
Oscar Frischmann <oscarfrisc@gmail.com>
2024-07-07 00:55:34 -0300

    index location button.firstChild.nodeValue

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,18 +1,22 @@
-import { indexImagesDB } from '../panel.js';
-const categoriesCont = document.getElementById('categories');
+import { indexImagesDB } from "../panel.js";
+const categoriesCont = document.getElementById("categories");
 for (let key in await indexImagesDB) {
-	console.log(indexImagesDB[key]);
-	const loader = document.getElementById('loader');
-	loader.style.display = 'none';
-	categoriesCont.innerHTML += `
+  console.log(indexImagesDB[key]);
+  const loader = document.getElementById("loader");
+  loader.style.display = "none";
+  categoriesCont.innerHTML += `
       <div class="col-sm p-2 position-relative">
         <img src=${indexImagesDB[key]} id="img" onload>
-        <button class="main-type-title" id="mujer">${key}</button>
+        <button class="main-type-title">${key}</button>
       </div>
     `;
 }
 
-const indexBTNs = document.querySelectorAll('.index button');
+const indexBTNs = document.querySelectorAll(".index button");
 indexBTNs.forEach((button, i) => {
-	button.setAttribute('onclick', `location.href ='./pages/${indexBTNs[i].attributes[1].nodeValue}.html'`);
+  console.log(button.firstChild.nodeValue);
+  button.setAttribute(
+    "onclick",
+    `location.href ='./pages/${button.firstChild.nodeValue}.html'`
+  );
 });
